c3c6901da5b7e45b3ad162ac6d99b735054f5652
Samani G. Gikandi <samani@gojulas.com>
2021-05-22 13:20:17 -0700

    Adds TODOs/FIXMEs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
 ///
 pub mod packet;
+
+// FIXME(async): Implement async read/write traits for types
+// FIXME(craft): Chew on header types
+// FIXME(craft): Make Flag bitfields optional on all packets
+// FIXME(craft): Preserve order of var arg fields
+// FIXME(perf): Consider making packet types zero-alloc
+// FIXME(craft): audit expect/unwrap usage on user-provided input
+// FIXME(craft): move common code out of individual packet modules
+// FIXME(craft): rename packet protocol
